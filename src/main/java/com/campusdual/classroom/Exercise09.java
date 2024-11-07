7f32ff10fee57598647aa0ecf80c637ee0e46ef9
valeriaterrone <valeriaprogramacioncurso@gmail.com>
2024-11-07 19:55:36 +0100

    Realizado y pasado el test.

--- src/main/java/com/campusdual/classroom/Exercise09.java
+++ src/main/java/com/campusdual/classroom/Exercise09.java
@@ -3,15 +3,20 @@ package com.campusdual.classroom;
 public class Exercise09 {
 
     public static void main(String[] args) {
-
+        doWhileLoop(15);
     }
 
-    //TODO ↓
     // Comprueba el funcionamiento del bucle do-while, que para cada interacción muestre el siguiente mensaje:
     // Entra con valor: X, sale con valor: X+1
     // Entra con valor: X+1, sale con valor: X+2;
     // etc.
     public static void doWhileLoop(int num) {
-
+        int ini = 0;
+        int primerValor = 0;
+        do {
+            System.out.println("Entra con valor: " + primerValor + ", sale con valor: " + (primerValor + 1));
+            primerValor++;
+            ini++;
+        } while (ini < num);
     }
 }
